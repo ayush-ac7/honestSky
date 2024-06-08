0fe9f0d24f364f6d7d41b3d75d162c59e4c8ba0c
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-06-08 15:47:36 +0530

    fetched weather API

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,54 @@
-const SearchBox = () => {
+import { useState } from "react";
+import { API_URL, API_KEY } from "../utils/constants";
+
+const SearchBox = ({ updateInfo }) => {
+  const [city, setCity] = useState("");
+  const [error, setError] = useState(false);
+
+  let getWeatherInfo = async () => {
+    try {
+      let data = await fetch(
+        `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      let json = await data.json();
+      console.log(json);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setCity("");
+      let newInfo = await getWeatherInfo();
+      updateInfo(newInfo);
+      console.log(city);
+    } catch {
+      setError(true);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center">
-      <input
-        type="text"
-        className="relative top-40 w-96 p-4 rounded-lg"
-        placeholder="Enter your City"
-      />
-      <button className="relative top-40 left-4 p-3 rounded-lg bg-blue-500 hover:opacity-90 text-white">
-        SEARCH
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="relative top-40 w-96 p-4 rounded-lg"
+          placeholder="Enter your City"
+          value={city}
+          onChange={handleChange}
+          required
+        />
+        <button className="relative top-40 left-4 p-3 rounded-lg bg-blue-500 hover:opacity-90 text-white">
+          SEARCH
+        </button>
+        {error && <p className="text-red-600">No such place exists</p>}
+      </form>
     </div>
   );
 };
