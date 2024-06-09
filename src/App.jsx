232434e7719549e0feeb6409058d14e9cfd073c8
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-06-09 18:04:48 +0530

    provided theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import Homepage from "./components/Homepage";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
-  return <>
-  <Homepage />
-  </>;
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <>
+        <Homepage />
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
