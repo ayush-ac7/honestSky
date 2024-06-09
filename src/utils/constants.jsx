6ff15fdc417031ab4a4071262874742b30f4e337
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-06-09 22:06:22 +0530

    added weather images url

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,10 @@
 export const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const API_KEY = "db660b1509cea22dec2a280f0b270879";
+
+export const HOT_URL =
+  "https://www.indiaspend.com/h-upload/2023/05/28/978286-extreme-heat-india-1500.webp";
+export const COLD_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGQK46NmWNOjSrvClLDgHMF9lEIUtzvCVs_HAOUCLjjA&s";
+export const RAIN_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAsBrkw2Gf7SCT9WHNvRm71ZWPMfk1-z-XbxikV0UANw&s";
