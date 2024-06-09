f3a61b1569e7354f9883ea327f3a653eb6b1f158
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-06-09 22:08:55 +0530

    added toggle theme btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import weatherImg from "../assets/nightweather.jpg";
+import weatherImg from "../assets/weather.jpg";
+import ThemeBtn from "./ThemeBtn";
 
 const Header = () => {
   return (
@@ -7,12 +8,16 @@ const Header = () => {
         <h1 className="absolute font-bold tracking-wider text-white z-10 m-2 p-2 text-4xl italic">
           honest<span className="text-sky-500">Sky</span>
         </h1>
+        
       </div>
       <img
         className="w-full h-56 absolute"
         src={weatherImg}
         alt="nightweather"
       />
+      <div className=" mr-10 pt-1 flex justify-end">
+          <ThemeBtn />
+        </div>
     </div>
   );
 };
