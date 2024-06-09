46c02d4b490d339985f10f010aa232b931e5f88d
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-06-09 08:36:21 +0530

    added initial dummy data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import Header from "./Header";
 import SearchBox from "./SearchBox";
+import WeatherInfo from "./WeatherInfo";
 
 const Homepage = () => {
+  const [weatherInfo, setWeatherInfo] = useState({
+    city:"Jhansi",
+    temp:30,
+    humidity:47,
+    feelsLike:32,
+    weather:"haze",
+    wind: 15,
+    gust: 2.7,
+    visibility:20000,
+    pressure: 1100,
+    grndlvl: 933,
+  });
+
+  const updateInfo = (newInfo) => {
+    setWeatherInfo(newInfo);
+  };
+
   return (
-    <div className="fullpage">
+    <div>
       <Header />
-      <SearchBox />
+      <SearchBox updateInfo={updateInfo} />
+      <WeatherInfo info={weatherInfo} />
     </div>
   );
 };
