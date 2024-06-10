9c1efb9daed279cca24db7aef37efa32ffb38b52
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-06-10 10:41:12 +0530

    improved UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,18 @@ const Header = () => {
         <h1 className="absolute font-bold tracking-wider text-white z-10 m-2 p-2 text-4xl italic">
           honest<span className="text-sky-500">Sky</span>
         </h1>
-        
       </div>
-      <img
-        className="w-full h-56 absolute"
-        src={weatherImg}
-        alt="nightweather"
-      />
-      <div className=" mr-10 pt-1 flex justify-end">
+
+      <div>
+        <div className="absolute right-3 left- mt-2">
           <ThemeBtn />
         </div>
+        <img
+          className="w-full h-56 static"
+          src={weatherImg}
+          alt="nightweather"
+        />
+      </div>
     </div>
   );
 };
