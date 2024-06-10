691fbd1f506d9fd0bd067a35195c029284538292
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-06-10 10:37:31 +0530

    updated urls

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -5,6 +5,6 @@ export const API_KEY = "db660b1509cea22dec2a280f0b270879";
 export const HOT_URL =
   "https://www.indiaspend.com/h-upload/2023/05/28/978286-extreme-heat-india-1500.webp";
 export const COLD_URL =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGQK46NmWNOjSrvClLDgHMF9lEIUtzvCVs_HAOUCLjjA&s";
+  "https://newsroompost.com/wp-content/uploads/2023/01/snowfall-srinagar-1-1000x600.jpg";
 export const RAIN_URL =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAsBrkw2Gf7SCT9WHNvRm71ZWPMfk1-z-XbxikV0UANw&s";
+  "https://img.etimg.com/thumb/width-1200,height-900,imgsize-222700,resizemode-75,msid-106065511/news/india/chennai-weather-update-heavy-rains-predicted-in-tamil-nadu-imd-issues-warning.jpg";
